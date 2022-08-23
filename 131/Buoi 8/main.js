66a7e57c6c18587b79643311ac0c7b1317ca68ff
tranngochai93nd <ngochai93.bbb@gmail.com>
2022-08-23 10:16:46 +0700

    html

--- 131/Buoi 8/main.js
+++ 131/Buoi 8/main.js
@@ -26,16 +26,17 @@
 
 
 
-// HOME WORK LESSON 8
+// ------HOME WORK LESSON 8----------
 
-let sao="";
-let sao1="";
-let khoang1="                        ";
-let khoang2="              ";
-for(let i=1;i<5;i++) {
-    console.log("*"+sao+khoang1+sao1+"*"+sao+khoang2+"*"+sao+"\n");
-    khoang1=khoang1.slice(0,-4);
-    khoang2=khoang2.slice(0,-2);
-    sao=sao+" *";
-    sao1=sao1+"* ";
-}
+// let star ="";
+// let star1="";
+// let khoang1="                        ";
+// let khoang2="              ";
+// for(let i=1;i<5;i++) {
+//     console.log("*"+star+khoang1+star1+"*"+star+khoang2+"*"+star+"\n");
+//     khoang1=khoang1.slice(0,-4);
+//     khoang2=khoang2.slice(0,-2);
+//     star+=" *";
+//     star1+="* ";
+// }
+
---
+++ 131/Buoi 9/main.js
@@ -0,0 +1,101 @@
+
+//----------HomeWork1
+// const myArray = [15,15,4,6,25,75];
+// function myFunctionMin(arr){
+//     let len = arr.length;
+//     let min = arr[0];
+//     for(let i=1 ; i<=len; i++){
+//         if (min > arr[i]) {
+//             min = arr[i];
+//         }
+//     }
+//     return min;
+// }
+// console.log(myFunctionMin(myArray));
+
+//---------- HomeWork2;
+// let n=100;
+// let m=100;
+// for (let i=2; i<=n; i++){
+//     let count=0;
+//     for (let j=1; j<=m;j++){
+//         if (i%j==0) {
+//             count++;
+//         }
+//     }
+//     if (count<=2){
+//         console.log(i);
+//     }
+// }
+
+//-----------Homework3;
+// const arr = [3,7,21,8,1,11,46,30];
+// let sum = arr.reduce(myFunction);//tinh tong day
+// function myFunction(total,value,index,array){
+//     return total + value;
+// }
+// console.log("Tong cua mang arr la:" + sum);
+
+// console.log("Day so chan la:");// tim so chan trong mang
+// function myFunction1 (arr1){
+//     for (let x of arr1) {
+//         if (x%2==0) {
+//             console.log(x);
+//         }
+//     }
+// }
+// myFunction1(arr);
+
+//----------Tinh tong so le trong mang
+// function myFunction1 (arr1){
+//     let sum =0;
+//     for (let x of arr1) {
+//         if (x%2!=0) {
+//             sum += x;
+//             }
+//         }
+//         return sum;
+//     }
+
+// console.log("Tong cac so le la:" + myFunction1(arr));
+
+//-------tinh tong gia tri <20
+// const newarr=arr.filter(myFunction2);
+// function myFunction2 (value) {
+//     return value <20
+// }
+// console.log(newarr);
+// function myFunction3 (total,value) {
+//     return total + value;
+// }
+// let y = newarr.reduce(myFunction3);
+// console.log("Tong so le gia tri nho hon 20 la:" + y);
+// const newarr1=arr.filter(myFunction4);
+// function myFunction4 (value) {
+//     return value >=15;
+// }
+// console.log(newarr1);
+
+
+
+// //Cach 1 tim min max, mang
+// console.log("max la:" + myFuntion4(arr));
+// console.log("min la:" + myFuntion5(arr));
+// function myFuntion4(arr2) {return Math.max.apply(null,arr2)}
+// function myFuntion5(arr2) {return Math.min.apply(null,arr2)}
+
+
+// // Cach 2 tim min va max mang
+//  arr.sort(myFunction6);
+//  function myFunction6(a,b) {return a-b;}
+// console.log("min day la:" + arr[0]);
+// console.log("max day la:" + arr[arr.length-1]);
+
+// cach 3: tu lap ham min,max
+
+// cho chuỗi, MindX , xuất ra mảng đảo ngược chiều lại
+const myArr = "MINX"  ;
+const newArr = Array.from(myArr);
+const newArr1 = newArr.reverse();
+const newArr2= newArr1.join("")
+console.log(newArr2);
